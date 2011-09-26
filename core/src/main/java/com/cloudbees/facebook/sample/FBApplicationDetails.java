125494f424e2a36c305567051029f848543b19d7
Stephen Connolly <stephen.alan.connolly@gmail.com>
2011-09-26 15:02:33 +0100

    initial commit

---
+++ core/src/main/java/com/cloudbees/facebook/sample/FBApplicationDetails.java
@@ -0,0 +1,9 @@
+package com.cloudbees.facebook.sample;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Named;
+
+@Named("fbApp")
+@ApplicationScoped
+public class FBApplicationDetails {
+}
---
+++ core/src/main/java/com/cloudbees/facebook/sample/P3PFilter.java
@@ -0,0 +1,33 @@
+package com.cloudbees.facebook.sample;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Ensures that the required P3P header is set.
+ */
+@WebFilter("P3PFilter")
+public class P3PFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+        HttpServletResponse rsp = (HttpServletResponse) response;
+        rsp.setHeader("P3P", "CP=\"IDC DSP COR ADM DEVi TAIi PSA PSD IVAi IVDi CONi HIS OUR IND CNT\"");
+        chain.doFilter(request, response);
+    }
+
+    @Override
+    public void destroy() {
+    }
+}
